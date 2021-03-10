1769de4e857703b46718f88c11d66183d7555330
eduardotorressubias <edu.torres9@hotmail.com>
2021-03-10 19:12:39 +0100

    explosion

---
+++ Assets/Scripts/VFX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float counterTime;
+    public float time;
+    public GameObject vfx;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterTime += Time.deltaTime;
+        if (counterTime >= time)
+        {
+            vfx.SetActive(true);
+        }
+        
+    }
+}
